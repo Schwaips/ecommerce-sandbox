af96fb91ab2a0431abe87124a14678b4fe045aeb
Schwaips <charlie.bertrand@live.com>
2025-05-29 12:59:21 +0200

    adding category in admin

---
+++ src/Controller/Admin/CategoryCrudController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CategoryCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Category::class;
+    }
+
+   
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name')
+                ->setRequired(true)
+                ->setLabel('Nom de la catégorie'),
+            SlugField::new('slug')
+                ->setRequired(true)
+                ->setLabel('URL')
+                ->setHelp('Le slug est utilisé dans l\'URL pour identifier la catégorie. Il doit être unique et ne contenir que des caractères alphanumériques, des tirets ou des underscores.')
+                ->setTargetFieldName('name')
+        ];
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Catégorie')
+            ->setEntityLabelInPlural('Catégories')
+            ->setPageTitle(Crud::PAGE_INDEX, 'Gestion des catégories')
+            ->setPageTitle(Crud::PAGE_EDIT, 'Modifier une catégorie')
+            ->setPageTitle(Crud::PAGE_NEW, 'Ajouter une catégorie')
+            ->setPageTitle(Crud::PAGE_DETAIL, 'Détails de la catégorie');
+    }
+}
