ca512aa48aef60f747eedd542c06b4343c5fae52
Schwaips <charlie.bertrand@live.com>
2025-05-29 13:34:17 +0200

    adding products to easy admin

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -5,10 +5,10 @@ namespace App\Entity;
 use App\Repository\ProductRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\UX\Turbo\Attribute\Broadcast;
+// use Symfony\UX\Turbo\Attribute\Broadcast;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
-#[Broadcast]
+// #[Broadcast]
 class Product
 {
     #[ORM\Id]
