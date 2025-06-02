7bc453aa56c21c3f29bc8403eb3fd0a4a28990fd
Schwaips <charlie.bertrand@live.com>
2025-06-02 13:06:07 +0200

    adding second step of order

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,11 +26,49 @@ final class OrderController extends AbstractController
       }
 
        $form = $this->createForm(OrderType::class, null, [
-         'addresses' => $addresses
+         'addresses' => $addresses,
+         'action' => $this->generateUrl('app_order_summary'),
        ]);
 
         return $this->render('order/index.html.twig', [
-            'deliveryForm' => $form->createView(),
+          'deliveryForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * 2eme étape de la commande - récapitulatif
+     * Insertion en base de données 
+     * Préparation du paiement vers stripe.
+     */
+    #[Route('/commande/recapitulatif', name: 'app_order_summary')]
+    public function add(Request $request, Cart $cart): Response
+    {
+      if($request->getMethod() != 'POST') {
+        return $this->redirectToRoute('app_order');
+      }
+    
+      $form = $this->createForm(OrderType::class, null, [
+        'addresses' => $this->getUser()->getAddresses()
+      ]);
+
+      $form->handleRequest($request);
+      if ($form->isSubmitted() && $form->isValid()) {
+          // Récupération des données du formulaire
+          $data = $form->getData();
+          $address = $data['addresses'];
+          $carrier = $data['carriers'];
+          // // Logique pour traiter la commande, par exemple :
+          // // - Enregistrer la commande en base de données
+          // // - Préparer le paiement avec Stripe
+
+          // // Redirection vers la page de paiement ou confirmation
+          // return $this->redirectToRoute('app_order_payment');
+      }
+
+        return $this->render('order/summary.html.twig', [
+          'choices' => $form->getData(),
+          'cart' => $cart->getCart(),
+          'totalCartWtprice' => $cart->getTotalWt(),
         ]);
     }
 }
