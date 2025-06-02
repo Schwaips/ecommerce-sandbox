3cea9180ef0f02e233acc1a7bcdc7016953c8424
Charlie Bertrand <66418712+Schwaips@users.noreply.github.com>
2025-06-02 11:50:06 +0200

    refactor(controllers): refacto Account controllers + product controller (#11)
    * refacto account controller
    
    * adding map entity to procuct controller

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,32 +2,26 @@
 
 namespace App\Controller;
 
-use App\Repository\ProductRepository;
+use App\Entity\Product;
+// use App\Repository\ProductRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
-    {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+  // public function show($slug, ProductRepository $productRepository): Response
+  #[Route('/produit/{slug}', name: 'app_product_show')]
+  public function show(#[MapEntity(mapping: ['slug' => 'slug'])] Product $product): Response
+  {
+    // $product = $productRepository->findOneBy(['slug' => $slug]);
+    if (!$product) {
+      return $this->redirectToRoute('app_home');
     }
 
-    #[Route('/produit/{slug}', name: 'app_product_show')]
-    public function show($slug, ProductRepository $productRepository): Response
-    {
-
-      $product = $productRepository->findOneBySlug($slug);
-      if (!$product) {
-        return $this->redirectToRoute('app_home');
-      }
-
-        return $this->render('product/show.html.twig', [
-            'product' => $product, // Placeholder for product name
-        ]);
-    }
+      return $this->render('product/show.html.twig', [
+          'product' => $product, // Placeholder for product name
+      ]);
+  }
 }
