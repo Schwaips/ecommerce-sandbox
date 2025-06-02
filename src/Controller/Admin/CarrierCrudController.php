0914a78c8a1e79452bf405883d4258a5df91ec56
Schwaips <charlie.bertrand@live.com>
2025-06-02 11:57:14 +0200

    adding transporteurs in easy admin

--- src/Controller/Admin/CarrierCrudController.php
+++ src/Controller/Admin/CarrierCrudController.php
@@ -3,9 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Carrier;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CarrierCrudController extends AbstractCrudController
@@ -15,14 +16,30 @@ class CarrierCrudController extends AbstractCrudController
         return Carrier::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name')
+                ->setRequired(true)
+                ->setLabel('Nom du transporteur'),
+            TextareaField::new('description')
+                ->setRequired(true)
+                ->setLabel('Description'),
+            NumberField::new('price')
+                ->setRequired(true)
+                ->setLabel('Prix')
+                ->setHelp('Le prix du transporteur en euros.'),
         ];
     }
-    */
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Transporteur')
+            ->setEntityLabelInPlural('Transporteurs')
+            ->setPageTitle(Crud::PAGE_INDEX, 'Gestion des transporteurs')
+            ->setPageTitle(Crud::PAGE_EDIT, 'Modifier un transporteur')
+            ->setPageTitle(Crud::PAGE_NEW, 'Ajouter un transporteur')
+            ->setPageTitle(Crud::PAGE_DETAIL, 'Détails de la catégorie');
+    }
 }
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Carrier;
 use App\Entity\Category;
 use App\Entity\Product;
 use App\Entity\User;
@@ -34,5 +35,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
         yield MenuItem::linkToCrud('Category', 'fas fa-list', Category::class);
         yield MenuItem::linkToCrud('Product', 'fas fa-list', Product::class);
+        yield MenuItem::linkToCrud('Transporteur', 'fas fa-list', Carrier::class);
     }
 }
